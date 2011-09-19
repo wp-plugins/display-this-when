1d20116f8a359d72496e435f8c8649b6e252c073
wolfiezero <wolfiezero@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-19 14:37:26 +0000

    Updating corrected version
    git-svn-id: https://plugins.svn.wordpress.org/display-this-when/trunk@440181 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display-this-when.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Display This When...
+Version: 1.0.1
+Plugin URI: http://wolfiezero.com/wordpress/display-this-when/
+Description: Display particular section of content within a post/page at a certain time and/day
+Author: WolfieZero
+Author URI: http://wolfiezero.com/
+*/
+ 
+add_shortcode( 'displaythis', 'displayThis' );
+
+function displayThis($atts, $content=null){
+	
+	$html = null;
+
+	$when	= $atts['when'];
+
+	if(isset($atts['format']))
+		$format	= $atts['format'];
+	else
+		$format = 'd-m-Y';
+
+	$date	= date($format);
+
+	if( $when == $date ) {
+		$html = $content;
+	} 
+
+	return $html;
+
+}
